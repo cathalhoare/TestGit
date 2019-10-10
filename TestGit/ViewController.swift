c2260a2efc81737df9080f670ba203d3f8357dc6
Cathal Hoare <cathalhoare@gmail.com>
2019-10-10 13:04:49 +0100

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Cathal Hoare on 10/10/2019.
+//  Copyright © 2019 Cathal Hoare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
